a3eb7ff7c53425580bef9e852ce63f3b8b8f9c46
Hongliang Li <hongliang.li@hotmail.com.au>
2021-04-19 12:19:13 +0000

    Add static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
@@ -20,7 +22,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
-
+    
     # path for dealer reviews view
 
     # path for add a review view
